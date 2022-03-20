ea8c850df2f3e3da0566c027326b07e4efae52ce
Yinxiao Wu <858459285@qq.com>
2022-03-20 16:53:14 +0800

    Add RangeModule

--- LCCode/solution.h
+++ LCCode/solution.h
@@ -4,4 +4,169 @@ using LL = long long;
 const int N = 2e5 + 7;
 const int INF = 0x3f3f3f3f;
 LL n,m,a,b;
+class Solution {
+public:
+    vector<int> longestRepeating(string s, string p, vector<int>& q) {
+        n = q.size();
+        vector<int> res(n,0);
+        if(n == 1) {
+            for(int i = 0;i < n;i ++) res[i] = 1;
+            return res;
+        }
+        m = s.size();
+        multiset<int> mst;// 存每一个长度
+        map<int,int> mp;// mp[r] = l;// 表示区间 [l:r]
+        for(int i = 0;i < m;i ++) {
+            int len = 1;
+            int l = i;
+            while(i + 1 < m && s[i + 1] == s[i]) len ++ , i++;
+            mst.insert(len);
+            mp[i] = l;
+        }
+        int idx = 0;
+        for(auto& pos : q) {
+            if(s[pos] == p[idx]) {// 不改变区间
+                res[idx] = *mst.rbegin();
+            } else {
+                // 区间
+                auto r = mp.lower_bound(pos)->first;// 找到 右端点
+                // [l,r] 是对应的区间
+                int l = mp[r];
+                int len = r - l + 1;
+                if(len == 1) {
+                    mst.erase(mst.find(1));// 删除当前区间
+                    if(pos && pos < n - 1) {
+                        // 在中间的情况
+                        // 等于左边和右边
+                        if(p[idx] == s[pos + 1] && p[idx] == s[pos - 1]) {
+                            auto rr = mp.lower_bound(pos + 1)->first;
+                            mst.erase(mst.find(rr - mp[rr] + 1));
+                            mst.erase(mst.find(r - mp[pos - 1]));
+                            int t = mp[pos - 1];
+                            mst.insert(rr - t + 1);
+                            mp[rr] = t;
+                            mp.erase(r);
+                            mp.erase(r - 1);
+                        }
+                        // 等于左边
+                        else if(p[idx] == s[pos - 1]) {
+                            int t = mp[l -1];
+                            mst.erase(mst.find(l - t));
+                            mst.insert(l - t + 1);
+                            mp.erase(l - 1);
+                            mp[r] = t;
+                        }
+                        // 等于右边
+                        else if(p[idx] == s[pos + 1]) {
+                            mp.erase(r);
+                            auto rr = mp.lower_bound(pos + 1)->first;
 
+                            mst.erase(rr - mp[rr] + 1);
+                            mst.insert(rr - r + 1);
+
+                            mp[rr] = r;
+                        }
+                        // 都不是
+                        else {
+                            mp[r] = r;
+                            mst.insert(1);
+                        }
+                    } else if(!pos) {
+                        if(p[idx] == s[pos + 1]) {
+                            mp.erase(r);
+                            auto rr = mp.lower_bound(pos + 1)->first;
+
+                            mst.erase(rr - mp[rr] + 1);
+                            mst.insert(rr - r + 1);
+
+                            mp[rr] = r;
+                        } else {
+                            mp[r] = r;
+                            mst.insert(1);
+                        }
+                    } else if(pos == n - 1) {
+                        if(p[idx] == s[pos - 1]) {
+                            int t = mp[l -1];
+                            mst.erase(mst.find(l - t));
+                            mst.insert(l - t + 1);
+                            mp.erase(l - 1);
+                            mp[r] = t;
+                        } else {
+                            mp[r] = r;
+                            mst.insert(1);
+                        }
+                    }
+                    res[idx] = *mst.rbegin();
+                    s[pos] = p[idx ++];
+                    continue;
+                }
+                mst.erase(mst.find(len));// 删除当前区间
+                if(pos > l && pos < r) {
+                    // 区间分割
+                    // [pos + 1,r]  [l,pos - 1] [pos,pos]
+                    mp[r] = pos + 1;
+                    mp[pos - 1] = l;
+                    mp[pos] = pos;
+                    mst.insert(r - pos);
+                    mst.insert(pos - l);
+                    mst.insert(1);
+                } else if(pos == l) {
+                    // 区间左端点
+                    if(l) {
+                        // 不是 0 端点 且和前面的一样
+                        if(p[idx] == s[l - 1]) {
+                            int t = mp[l -1];
+                            mst.erase(mst.find(l - t));
+                            mst.insert(l - t + 1);
+                            mp.erase(l - 1);
+                            mp[l] = t;
+                            mp[r] = l + 1;
+                            mst.insert(len - 1);
+                        } else {
+                            // 和前面不一样
+                            mp[l] = l;
+                            mst.insert(1);
+                            mst.insert(len - 1);
+                            mp[r] = l + 1;
+                        }
+                    } else {
+                        mp[l] = l;
+                        mst.insert(1);
+                        mst.insert(len - 1);
+                        mp[r] = l + 1;
+                    }
+                } else if(pos == r) {
+                    // 不是最右端点
+                    if(r != n - 1) {
+                        // 不是 n - 1 端点 且和后面的一样
+                        if(p[idx] == s[r + 1]) {
+                            auto rr = mp.lower_bound(r + 1)->first;
+
+                            mst.erase(rr - mp[rr] + 1);
+                            mst.insert(rr - r + 1);
+
+                            mp[rr] = r;
+
+                            mst.insert(len - 1);
+                            mp[r - 1] = l;
+                        } else {
+                            // 和后面不一样
+                            mp[r] = r;
+                            mst.insert(len - 1);
+                            mst.insert(1);
+                            mp[r - 1] = l;
+                        }
+                    } else {
+                        mp[r] = r;
+                        mst.insert(len - 1);
+                        mst.insert(1);
+                        mp[r - 1] = l;
+                    }
+                }
+                res[idx] = *mst.rbegin();
+            }
+            s[pos] = p[idx ++];
+        }
+        return res;
+    }
+};
---
+++ TEMPLATE/RangeModule/RangeModule.h
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using PII = pair<int,int>;
+class RangeModule
+{
+    /***
+     * 适用范围 : 每一段小区间左右端点合并在一起是一段完整的区间
+    */
+private:
+    multiset<int> mst;// 每一段区间长度
+    map<int,PII> mp;// mp[r] = {l,quality}
+    int l_,r_;// 总区间的范围 [l_,r_]
+public:
+    void erase(int r) {// 删除端点 r 的区间
+        mst.erase(mst.find(r - mp[r].first + 1)) , mp.erase(r);
+    }
+    void add(int l,int r,int q) {// 加入区间 [l:r]
+        if(r < l) return ;
+        mst.insert(r - l + 1) , mp[r] = {l,q};
+    }
+    int getMaxRLen() {// 返回区间的最大值
+        return *mst.rbegin();
+    }
+    int getRange(int pos) {// 得到 pos 的所属区间的右端点
+        return mp.lower_bound(pos)->first;
+    }
+    void update(int pos,int nq) {// 将位置 pos 的属性改为 nq
+        // 需要判断 pos 在所属区间 [l:r]
+        int r = getRange(pos) , l = mp[r].first , q = mp[r].second;
+        if(q == nq) return ;// 该点的属性不变
+        // 然后判断 pos 在 [l:r] 的方位
+
+        if(r == l && l > l_ && l < r_ && mp[l - 1].second == nq) {// 取左右两个区间判断是否能合并
+            auto rr = getRange(r + 1);// 右边的区间的右端点
+            if(mp[rr].second == nq) {
+                // [mp[l-1].first,l-1] + [l,r] + [r+1,rr] -> [mp[l-1].first,rr]
+                int ll = mp[l-1].first;
+                erase(l-1) , erase(r) , erase(rr);
+                add(ll,rr,nq);
+                return ;
+            }
+        }
+        if(pos > l && pos < r) {
+            // [l,r] -> [l,pos - 1] [pos,pos] [pos + 1,r] 前后两个区间的属性不变
+            erase(r) , add(l,pos - 1,q) , add(pos,pos,nq) , add(pos + 1,r,q);
+        }
+        bool merge = false;
+        if(pos == l) {
+            // 判断是否和左边的区间属性相同
+            if(pos == l_) {// 已经是最左边的区间了
+                // [l,r] -> [l,l] + [l + 1,r] 
+                erase(r);
+                add(l,l,nq) , add(l + 1,r,q);
+            } else {
+                if(mp[l-1].second == nq) {
+                    // [mp[l-1].first,l-1] + [l:r] -> [mp[l-1].first,l] + [l+1,r]
+                    int ll = mp[l - 1].first;
+                    erase(r) , erase(l - 1);
+                    add(ll,l,nq) , add(l + 1,r,q);
+                    merge = true;
+                } else {
+                    // [l,r] -> [l,l] + [l + 1,r] 
+                    erase(r);
+                    add(l,l,nq) , add(l + 1,r,q);
+                }
+            }
+        }
+        if(pos == r) {
+            // 判断是否和右边的区间属性相同
+            if(merge) return ;// 如果和左边的进行合并过了
+            if(r == r_) {// 已经是最右边的区间
+                // [l,r] -> [l,r - 1] + [r,r] 
+                erase(r);
+                add(r,r,nq) , add(l,r - 1,q);
+            } else {
+                int rr = getRange(r + 1);
+                if(mp[rr].second == nq) {
+                    // [l,r] + [mp[rr].first,rr] -> [l,r - 1] + [r,rr]
+                    erase(r) , erase(rr);
+                    add(l,r - 1,q) , add(r,rr,nq);
+                } else {
+                    // [l,r] -> [l,r - 1] + [r,r] 
+                    erase(r);
+                    add(r,r,nq) , add(l,r - 1,q);
+                }
+            }
+        }
+    }
+    RangeModule(int _l,int _r) {
+        l_ = _l , r_ = _r;
+    };
+};
